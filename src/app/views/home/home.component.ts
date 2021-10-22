5f4c21cfc74f0c7eb653a38dd9e4e1a5210c495c
Didierson AMURI <didiamuri@gmail.com>
2021-10-22 14:01:22 +0100

    Adding Github User API finder

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../../interfaces/user";
-import {Observable} from "rxjs";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {UserService} from "../../services/user.service";
 import {first} from "rxjs/operators";
